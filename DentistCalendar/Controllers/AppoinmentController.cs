d831dbfef7965d3d51a4a4f89b060db95063e5bc
EFDAL AKIN BARSAN <61588170+efdalbarsan@users.noreply.github.com>
2021-04-20 11:33:19 +0300

    AKTİVASYONLARIN OLUSTURLMASI

---
+++ DentistCalendar/Controllers/AppoinmentController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DentistCalendar.Data;
+using DentistCalendar.Data.Entity;
+using DentistCalendar.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DentistCalendar.Controllers
+{
+    public class AppoinmentController : Controller
+    {
+        private ApplicationDbContext _context;
+        public AppoinmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+      
+        public JsonResult GetAppoinments()
+        {
+            var model = _context.Appointments
+                .Include(x => x.User).Select(x => new AppointmentViewModel()
+                {
+                    Dentist = x.User.Name + " " + x.User.Surname,
+                    Patient = x.PatientName+ " " + x.PatientSurname,
+                    StartDate = x.StartDate,
+                    EndDate = x.EndDate,
+                    Description = x.Description,
+                    UserId = x.User.Id
+                });
+
+            return Json(model);
+        }
+
+        public JsonResult GetAppoinmentsByDentist(string userId = "")
+        {  
+            var model = _context.Appointments.Where(x => x.UserId == userId)
+                .Include(x => x.User).Select(x => new AppointmentViewModel()
+                {
+                    Dentist = x.User.Name + " " + x.User.Surname,
+                    Patient = x.PatientName + " " + x.PatientSurname,
+                    StartDate = x.StartDate,
+                    EndDate = x.EndDate,
+                    Description = x.Description,
+                    UserId = x.User.Id
+                });
+
+            return Json(model);
+        }
+
+        [HttpPost]
+        public JsonResult AddOrUpdateAppoinment(AddOrUpdateAppointmentModel model)
+        {
+            //Validasyon
+            if (model.Id == 0)
+            {
+                Appointment entity = new Appointment()
+                {
+                    CreatedDate = DateTime.Now,
+                    StartDate = model.StartDate,
+                    EndDate = model.EndDate,
+                    PatientName = model.PatientName,
+                    PatientSurname = model.PatientSurname,
+                    Description = model.Description,
+                    UserId = model.UserId
+                };
+
+                _context.Add(entity);
+                _context.SaveChanges();
+            }
+            else
+            {
+                var entity = _context.Appointments.SingleOrDefault(x => x.Id == model.Id);
+                if (entity == null)
+                {
+                    return Json("Güncellenecek veri bulunamadı.");
+                }
+                entity.UpdatedDate = DateTime.Now;
+                entity.PatientName = model.PatientName;
+                entity.PatientSurname = model.PatientSurname;
+                entity.Description = model.Description;
+                entity.StartDate = model.StartDate;
+                entity.EndDate = model.EndDate;
+                entity.UserId = model.UserId;
+
+                _context.Update(entity);
+                _context.SaveChanges();
+            }
+
+            return Json("200");
+        }
+
+        public JsonResult DeleteAppointment(int id = 0)
+        {
+            var entity = _context.Appointments.SingleOrDefault(x => x.Id == id);
+            if (entity == null)
+            {
+                return Json("Kayıt bulunamadı.");
+            }
+            _context.Remove(entity);
+            _context.SaveChanges();
+            return Json("200");
+        }
+    }
+}
--- DentistCalendar/Models/AddOrUpdateAppointmentModel.cs
+++ DentistCalendar/Models/AddOrUpdateAppointmentModel.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DentistCalendar.Data.Entity
+namespace DentistCalendar.Models
 {
-    public class Appointment
+    public class AddOrUpdateAppointmentModel
     {
         public int Id { get; set; }
         public string UserId { get; set; }
-        public AppUser User { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string PatientName { get; set; }
